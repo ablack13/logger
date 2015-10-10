392d3ae69a8044af6424907954a66cf1316b34e9
scijoker <scijoker@gmail.com>
2015-10-10 09:56:22 +0300

    Update Logger.java

--- code/Logger.java
+++ code/Logger.java
@@ -14,40 +14,40 @@ public class Logger {
     private static boolean DEBUG = true;
 
     public static void d(String tag, String arg) {
-        if (isDebugEnable()) {
-            Log.d(tag, arg);
+        if (isEnable()) {
+            log(tag, arg);
         }
     }
 
     public static void d(String logMsg) {
-        if (isDebugEnable()) {
-            d(getCurrentClassName(), getCurrentMethodName() + "(): " + logMsg);
-        }
-    }
-
-    public static void dd(String tag, JSONObject jsonObject) {
-        if (isDebugEnable()) {
-            try {
-                format(tag, jsonObject.toString(2));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        if (isEnable()) {
+            log(getCurrentClassName(), getCurrentMethodName() + "(): " + logMsg);
         }
     }
 
     public static void dd(String tag, Object source) {
-        if (!isDebugEnable()) {
-            return;
-        }
-        if (containsJsonStr(source)) {
-            try {
-                dd(tag, new JSONObject(source.toString()).toString(2));
-            } catch (JSONException e) {
-                e.printStackTrace();
+        if (isEnable()) {
+            Object o = containsJsonStr(source);
+            if (o != null) {
+                try {
+                    if (o instanceof JSONObject) {
+                        format(tag, ((JSONObject) o).toString(2));
+                    } else if (o instanceof JSONArray) {
+                        format(tag, ((JSONArray) o).toString(2));
+                    } else {
+                        format(tag, source);
+                    }
+                } catch (JSONException e) {
+                    format(tag, source);
+                }
+            } else {
+                format(tag, source);
             }
-        } else {
-            format(tag, source);
         }
+    }
+
+    private static void log(String tag, String msg) {
+        Log.d(tag, msg);
     }
 
     private static String getSplitter(int length) {
@@ -60,11 +60,11 @@ public class Logger {
 
     private static void format(String tag, Object source) {
         tag = " " + tag + " ";
-        d(" ", " ");
-        d(" ", getSplitter(50) + tag + getSplitter(50));
-        d(" ", "" + source);
-        d(" ", getSplitter(100 + tag.length()));
-        d(" ", " ");
+        log(" ", " ");
+        log(" ", getSplitter(50) + tag + getSplitter(50));
+        log(" ", "" + source);
+        log(" ", getSplitter(100 + tag.length()));
+        log(" ", " ");
     }
 
     private static String getCurrentMethodName() {
@@ -73,28 +73,28 @@ public class Logger {
 
     private static String getCurrentClassName() {
         String className = Thread.currentThread().getStackTrace()[4].getClassName();
-
         String[] temp = className.split("[\\.]");
         className = temp[temp.length - 1];
         return className;
     }
 
-    private static boolean containsJsonStr(Object test) {
+    private static Object containsJsonStr(Object test) {
+        Object o = null;
         try {
-            new JSONObject(test.toString());
+            o = new JSONObject(test.toString());
         } catch (JSONException ex) {
             try {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    new JSONArray(test);
+                    o = new JSONArray(test);
                 }
             } catch (JSONException ex1) {
                 return false;
             }
         }
-        return true;
+        return o;
     }
 
-    private static boolean isDebugEnable() {
+    public static boolean isEnable() {
         return DEBUG;
     }
 
